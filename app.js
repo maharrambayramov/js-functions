a3e9a74974288a6d9b18cf7fe80aa175db4321ad
maharrambayramov <meherrem.bayramov.03@gmal.com>
2023-04-20 01:38:00 +0400

    updated task 2

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-function power(num, square) {
-    var result = 1;
-    if (square === 0) {
-        return 1;
-    } else {
-        for (var i = 1; i <= square; i++) {
-            if (i % 2 === 0) {
-                result *= result;
-            } else {
-                result *= num;
-            }
-        }
-        return result;
-    }
-}
-console.log(power(2, 5));
+// function power(num, square) {
+//     var result = 1;
+//     if (square === 0) {
+//         return 1;
+//     } else {
+//         for (var i = 1; i <= square; i++) {
+//             if (i % 2 === 0) {
+//                 result *= result;
+//             } else {
+//                 result *= num;
+//             }
+//         }
+//         return result;
+//     }
+// }
+// console.log(power(5, 2));
 
+function sum(num) {
+    if (num % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+
+}
+console.log(sum(5));
